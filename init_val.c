e41a3c93e948f5fe944766cc67786e3f2dea0459
Patryk Chodur <patrykchodur@icloud.com>
2020-12-13 15:36:26 +0100

    init_val.c added volumes levels

--- init_val.c
+++ init_val.c
@@ -16,7 +16,7 @@ double triangle(double val) {
 }
 
 void usage(const char *program_name) {
-	fprintf(stderr, "Usage: %s [-s samples_no]\n", program_name);
+	fprintf(stderr, "Usage: %s [-s samples_no] [-a amplitudes_no]\n", program_name);
 }
 
 int main(int argc, char *argv[]) {
@@ -25,11 +25,15 @@ int main(int argc, char *argv[]) {
 
 	int ch;
 	int samples_no = 16;
+	int amplitudes_no = 1;
 
-	while ((ch = getopt(argc, argv, "s:")) != -1) {
+	while ((ch = getopt(argc, argv, "s:a:")) != -1) {
 		switch (ch) {
 		case 's':
 			sscanf(optarg, "%d", &samples_no);
+			break;
+		case 'a':
+			sscanf(optarg, "%d", &amplitudes_no);
 			break;
 		default:
 			usage(argv[0]);
@@ -37,12 +41,21 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	for (int iter = 0; iter < samples_no; ++iter) {
-		int8_t sin_val = INT8_MAX * sin(iter * 2. * M_PI / samples_no);
-		int8_t triangle_val = INT8_MAX * triangle(iter * 2. * M_PI / samples_no);
+	for (int amp_iter = 0; amp_iter < amplitudes_no; ++amp_iter) {
+		int nominator = amplitudes_no == 1 ? 1 : amp_iter;
+		int denominator = amplitudes_no == 1 ? 1 : amplitudes_no - 1;
+		for (int iter = 0; iter < samples_no; ++iter) {
+			int8_t sin_val = INT8_MAX * sin(iter * 2. * M_PI / samples_no);
+			int8_t triangle_val = INT8_MAX * triangle(iter * 2. * M_PI / samples_no);
 
-		fprintf(sin_file, "%x ", (uint8_t)sin_val);
-		fprintf(triangle_file, "%x ", (uint8_t)triangle_val);
+			sin_val = nominator * sin_val / denominator;
+			triangle_val = nominator * sin_val / denominator;
+
+			fprintf(sin_file, "%2x ", (uint8_t)sin_val);
+			fprintf(triangle_file, "%2x ", (uint8_t)triangle_val);
+		}
+		fprintf(sin_file, "\n");
+		fprintf(triangle_file, "\n");
 	}
 
 	fclose(sin_file);
